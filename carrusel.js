c3fef77d8fcf0edb91b7a985d443dc70e539708e
Cristian Barrantes <cristiandimas74@gmail.com>
2022-11-08 21:40:50 -0500

    Subiendo e-commerce

---
+++ carrusel.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+punto.forEach( (cadaPunto, i)=>{
+  
+  punto[i].addEventListener('click',()=>{
+    let posicion = i     
+    let operacion = posicion * -14.28
+    
+     console.log(operacion)
+    grande.style.transform = `translateX(${operacion}%)`    
+  })
+})
+
